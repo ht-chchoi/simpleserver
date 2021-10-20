0bd2717f058ed047a8c9c62781e0e1f9e3e42df4
ht-chchoi <chchoi@hdtel.co.kr>
2021-10-20 15:50:32 +0900

    jdk8 test 2

--- src/main/java/com/example/simpleserver/controller/CheckController.java
+++ src/main/java/com/example/simpleserver/controller/CheckController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckController {
   @GetMapping
   public Object checkIsAlive() {
-    return Map.of("code", "200", "message", "ok");
+//    return Map.of("code", "200", "message", "ok");
+    return "{\"code\": \"200\", \"message\": \"ok\"}";
   }
 }
